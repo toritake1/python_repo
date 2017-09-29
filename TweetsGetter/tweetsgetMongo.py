16f8b6995027310f2de2a6f35a13ce6dbd4de3f8
root <root@stg12-jenkins12.levorg.local>
2017-09-29 16:27:59 +0900

    add

--- TweetsGetter/tweetsgetMongo.py
+++ TweetsGetter/tweetsgetMongo.py
@@ -2,24 +2,30 @@
  
 from pymongo import MongoClient
 from twitterAPI import TweetsGetter
- 
 import json
+import datetime
+from dateutil import parser
+from pytz import timezone
+import pandas as pd
 
 client = MongoClient('localhost', 27017)
+db = client.twitter
  
-db = client.tweetsget
- 
-#tweets = TweetsGetter.byUser('idcfrontier').collect(total=10)
-tweets = TweetsGetter.byUser('sugitaLOV').collect(total=20)
-for tweet in tweets:
-#    tweet2 = {}
+today = datetime.date.today()
+date = today.strftime("%Y-%m-%d")
+
+df = pd.read_csv('/usr/local/lib/python/tweepy/user.csv')
+for sn in df['screen_name']:
+   for tweet in TweetsGetter.byUser(sn).collect(total=1):
+       tweet2 = {}
 #    tweet2['id'] = tweet['id']
 #    tweet2['id_str'] = tweet['id_str']
 #    tweet2['text'] = tweet['text']
 #    tweet2['created_at'] = tweet['created_at']
-#    tweet2['screen_name'] = tweet['user']['screen_name']
-#    tweet2['name'] = tweet['user']['name'] 
-#    tweet2['followers_count'] = tweet['user']['followers_count']
+       tweet2['screen_name'] = tweet['user']['screen_name']
+       tweet2['name'] = tweet['user']['name'] 
+       tweet2['followers_count'] = tweet['user']['followers_count']
+       tweet2['date'] = date
 
-#    db.tweet.insert(tweet2)
-     db.tweet.insert(tweet)
+       db.followers.insert(tweet2)
+       #print tweet2
--- TweetsGetter/tweetsgetMongoRead.py
+++ TweetsGetter/tweetsgetMongoRead.py
@@ -4,11 +4,12 @@ from pymongo import MongoClient
  
 client = MongoClient('localhost', 27017)
  
-db = client.tweetsget
+db = client.twitter
  
 cnt = 0
-for record in db.tweet.find():
+
+for record in db.followers.find():
     cnt += 1
     print '----',cnt
-    print record['text']
+    print record
 
