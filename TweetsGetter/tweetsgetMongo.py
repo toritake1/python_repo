ec5995ac44131d70d1ed26adc7f6073fbbb6864f
root <root@stg12-jenkins12.levorg.local>
2017-10-04 16:26:30 +0900

    tweetsgetMongo.py

--- TweetsGetter/tweetsgetMongo.py
+++ TweetsGetter/tweetsgetMongo.py
@@ -11,21 +11,16 @@ import pandas as pd
 client = MongoClient('localhost', 27017)
 db = client.twitter
  
-today = datetime.date.today()
-date = today.strftime("%Y-%m-%d")
-
-df = pd.read_csv('/usr/local/lib/python/tweepy/user.csv')
-for sn in df['screen_name']:
-   for tweet in TweetsGetter.byUser(sn).collect(total=1):
+df = pd.read_csv('user.csv')
+for sn in df['user_name']:
+   for tweet in TweetsGetter.bySearch(sn).collect(total=10):
        tweet2 = {}
-#    tweet2['id'] = tweet['id']
-#    tweet2['id_str'] = tweet['id_str']
-#    tweet2['text'] = tweet['text']
-#    tweet2['created_at'] = tweet['created_at']
+       tweet2['id'] = tweet['id']
+       tweet2['id_str'] = tweet['id_str']
+       tweet2['text'] = tweet['text']
+       tweet2['created_at'] = tweet['created_at']
        tweet2['screen_name'] = tweet['user']['screen_name']
        tweet2['name'] = tweet['user']['name'] 
-       tweet2['followers_count'] = tweet['user']['followers_count']
-       tweet2['date'] = date
 
-       db.followers.insert(tweet2)
+       db.search.insert(tweet2)
        #print tweet2
